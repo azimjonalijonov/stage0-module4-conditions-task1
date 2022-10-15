7e121179b7f3e4c2c33ab9834812130aa412aad0
azimjonalijonov <115147667+azimjonalijonov@users.noreply.github.com>
2022-10-15 15:55:23 +0500

    Update ConditionalTruePrinter.java

--- src/main/java/school/mjc/stage0/conditions/task1/ConditionalTruePrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/ConditionalTruePrinter.java
@@ -3,6 +3,6 @@ package school.mjc.stage0.conditions.task1;
 public class ConditionalTruePrinter {
     public static void main(String[] args) {
         if(true){
-         System.out.println(true);
+         System.out.println(true);}
     }
 }
