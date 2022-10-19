5cf266a933440304f2ec2262d8b0dc7ed8622254
alijonoff <2110040@newuzbekistanuniversity.uz>
2022-10-20 00:20:30 +0500

    d

--- src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
@@ -2,10 +2,10 @@ package school.mjc.stage0.conditions.task1;
 
 public class ConditionalFalsePrinter {
     public static void main(String[] args) {
-        if(false){
-         System.out.println(true);
-  
-        }else{
+        if (false) {
+            System.out.println(true);
+
+        } else {
             System.out.println(false);
-    }
-}
+        }
+    }}
